70d4691468907f8e950d148b18474b5906c44abd
Ahmed Mahmud <ahmedmahmud12971@gmail.com>
2016-08-22 01:18:33 +0500

    Initial Commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from './HelloWorld.jsx';
+
+class App extends React.Component {
+  render() {
+    return <HelloWorld />
+  }
+}
+
+export default App;
